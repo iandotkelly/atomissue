5ed184e65cda116763807364ebbadf3b6da80a3e
Ian Kelly <iandotkelly@gmail.com>
2014-05-27 15:43:22 -0400

    Initial commit

---
+++ sample.js
@@ -0,0 +1,17 @@
+/**
+ * @description Stuff
+ */
+
+'use strict';
+
+/**
+ * Route for GET /api/users - returns the current user's profile
+ */
+function retrieve() {
+
+	// Does stuff
+}
+
+module.exports = {
+	retrieve: retrieve
+};
